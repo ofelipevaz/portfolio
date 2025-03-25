b449869a30f8ec5c52038c23eb9af4fe95fc4432
ofelipevaz <felipeisvz@gmail.com>
2025-03-25 16:45:57 -0300

    chore: add tailwind configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: false,
+      screens: [],
+    },
+    extend: {},
+  },
+}
+
+export default config
