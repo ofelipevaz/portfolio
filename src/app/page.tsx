779b3cc152ac7780d482ecb02172a3212307f01d
ofelipevaz <felipeisvz@gmail.com>
2025-03-27 11:48:23 -0300

    feat: add text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Text } from "@/components/@ui/text"
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        magni explicabo at asperiores quis fugiat enim mollitia animi quae,
+        quisquam aliquam nulla earum neque ex error tempora ab cumque suscipit.
+      </Text>
+      <Text as="blockquote" variants={{ decoration: "underline" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        magni explicabo at asperiores quis fugiat enim mollitia animi quae,
+        quisquam aliquam nulla earum neque ex error tempora ab cumque suscipit.
+      </Text>
+    </div>
+  )
 }
