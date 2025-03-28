a9e49384e2fe91ab227cb8278cf1645e0120529e
ofelipevaz <felipeisvz@gmail.com>
2025-03-28 16:04:52 -0300

    feat: add header side buttons component

---
+++ src/components/-header/SideButtons/index.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Link from "next/link"
+import { Separator } from "~/src/ui/separator"
+import { ScreenReaderOnly } from "~/src/ui/screen-reader-only"
+import { HeaderLanguageSwitcher } from "../LanguageSwitcher"
+
+import { useTheme } from "next-themes"
+
+import { FaGithub } from "react-icons/fa"
+import { LuSunMedium, LuSunMoon } from "react-icons/lu"
+
+import "./index.styles.css"
+
+export function HeaderSideButtons() {
+  const { theme, setTheme } = useTheme()
+
+  function handleChangeThemeClick() {
+    setTheme(theme == "dark" ? "light" : "dark")
+  }
+
+  return (
+    <div className="side-buttons">
+      <div className="buttons">
+        <button
+          onClick={handleChangeThemeClick}
+          className="change-theme-button"
+        >
+          <ScreenReaderOnly>Mudar tema</ScreenReaderOnly>
+          {theme == "dark" ? <LuSunMedium /> : <LuSunMoon />}
+        </button>
+        <Link
+          className="github-button"
+          target="_blank"
+          href="https://github.com/ofelipevaz"
+        >
+          <FaGithub />
+          <ScreenReaderOnly>GitHub</ScreenReaderOnly>
+        </Link>
+      </div>
+      <Separator variants={{ orientation: "vertical" }} />
+      <HeaderLanguageSwitcher />
+    </div>
+  )
+}
