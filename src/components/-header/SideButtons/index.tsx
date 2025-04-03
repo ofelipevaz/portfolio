575a3b0506eeed40055449eb27438b0cc83bb8ca
ofelipevaz <felipeisvz@gmail.com>
2025-04-03 12:17:39 -0300

    feat: add change theme toast and more acessibility

--- src/components/-header/SideButtons/index.tsx
+++ src/components/-header/SideButtons/index.tsx
@@ -12,27 +12,45 @@ import { LuSunMedium, LuSunMoon } from "react-icons/lu"
 
 import "./index.styles.css"
 import { useTranslations } from "next-intl"
+import { useToast } from "~/src/hooks/useToast"
 
 export function HeaderSideButtons() {
   const { theme, setTheme } = useTheme()
-  const t = useTranslations("sections.Header")
+  const { notify } = useToast()
+  const t = useTranslations()
 
   function handleChangeThemeClick() {
-    setTheme(theme == "dark" ? "light" : "dark")
+    const newTheme = theme == "dark" ? "light" : "dark"
+    const themeTranslation = {
+      light: t("sections.Header.themes.light"),
+      dark: t("sections.Header.themes.dark"),
+    }
+
+    setTheme(newTheme)
+    notify({
+      title: t("toasts.themeChanged.title"),
+      description: t("toasts.themeChanged.description", {
+        theme: themeTranslation[newTheme],
+      }),
+    })
   }
 
   return (
     <div className="header-side-buttons">
       <div className="buttons">
         <button
+          title={t("sections.Header.changeThemeButtonLabel")}
           onClick={handleChangeThemeClick}
           className="change-theme-button"
         >
-          <ScreenReaderOnly>{t("changeThemeButtonLabel")}</ScreenReaderOnly>
+          <ScreenReaderOnly>
+            {t("sections.Header.changeThemeButtonLabel")}
+          </ScreenReaderOnly>
           {theme == "dark" ? <LuSunMedium /> : <LuSunMoon />}
         </button>
         <Link
           className="github-button"
+          title="GitHub"
           target="_blank"
           href="https://github.com/ofelipevaz"
         >
